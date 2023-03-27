e87be569b6d809e1b7cf952dbb6c7f1d7cff347d
Dario Milani <dario.milani@abstract.it>
2023-03-27 17:42:40 +0200

    Updates

---
+++ precache-manifest.ecac351636de0080d3dba4711d47c71a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa68665441b573b0f99",
+    "url": "/beer_app/static/css/main.a2d1e9fa.chunk.css"
+  },
+  {
+    "revision": "4aa68665441b573b0f99",
+    "url": "/beer_app/static/js/main.4aa68665.chunk.js"
+  },
+  {
+    "revision": "273da43f13bf096ea852",
+    "url": "/beer_app/static/js/1.273da43f.chunk.js"
+  },
+  {
+    "revision": "0f6dba34bc70fda0e084",
+    "url": "/beer_app/static/js/runtime~main.0f6dba34.js"
+  },
+  {
+    "revision": "fe0908987fe731522a15c6d5ac261f1b",
+    "url": "/beer_app/static/media/brewdog.fe090898.png"
+  },
+  {
+    "revision": "826f4870c9f4bbacab29418bc1590a4e",
+    "url": "/beer_app/index.html"
+  }
+];
